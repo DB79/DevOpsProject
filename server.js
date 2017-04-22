d1cd216e461cebf388e7bbd458693eb8b1ffd72a
Damien Breen <damienbreen7@gmail.com>
2017-04-22 16:24:09 +0100

    testing without server.js

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-var express = require("express");
-var bodyParser = require("body-parser");
+// var express = require("express");
+// var bodyParser = require("body-parser");
 
-var app = express();
-app.use(bodyParser.json());
+// var app = express();
+// app.use(bodyParser.json());
 
-// Create link to Angular build directory
-var distDir = __dirname + "/dist/";
-app.use(express.static(distDir));
+// // Create link to Angular build directory
+// var distDir = __dirname + "/dist/";
+// app.use(express.static(distDir));
 
-var server = app.listen(process.env.PORT || 8080, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
-});
+// var server = app.listen(process.env.PORT || 8080, function () {
+//     var port = server.address().port;
+//     console.log("App now running on port", port);
+// });
